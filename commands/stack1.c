221bc882d07c66f7465772be399a1f9ef2a2bed1
Beth Laptop <beefie@Beefie>
2024-07-23 17:20:57 +0930

    removed smp files

--- commands/stack1.c
+++ commands/stack1.c
@@ -6,10 +6,70 @@
 /*   By: beefie <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 11:22:51 by beefie            #+#    #+#             */
-/*   Updated: 2024/06/26 11:33:52 by beefie           ###   ########.fr       */
+/*   Updated: 2024/07/23 17:13:59 by beefie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 
+
+void	swap(t_list **stack)
+{
+	t_list	*first;
+	t_list	*second;
+
+	if (!*stack || (*stack)->next)
+		return ;
+	first = *stack;
+	second = (*stack)->next;
+	*stack = second;
+	first->next = second->next;
+	second->next = first;
+}
+
+void	push(t_list **stacka,t_list **stackb)
+{
+	t_list	*first;
+	t_list	*fromb;
+
+	if (!*stackb)
+		return ;
+	fromb = *stackb;
+	first = *stacka
+	*stackb = (*stackb)->next;
+	*stacka = fromb;
+	fromb->next = first;
+}
+
+void	rotate(t_list **stack)
+{
+	t_list	*first;
+	t_list	*last;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	first = *stack;
+	*stack = (*stack)->next;
+	last = ft_lstlast(*stack);
+	last->next = first;
+	first->next = NULL;
+}
+
+void	reverse_rotate(t_list **stack)
+{
+	t_list	*first;
+	t_list	*last;
+	t_list	*secondlast;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	first = *stack;
+	secondlast = *stack
+	while (secondlast->next->next != NULL)
+		secondlast = secondlast->next;
+	last = secondlast->next;
+	*stack = last;
+	last->next = first;
+	secondlast->next = NULL;
+}
